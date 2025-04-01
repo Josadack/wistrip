06424e4a45fd17d7138cc59d0490c62af1ccdd5c
Josadack <josadaqueferreira@outlook.com>
2025-04-01 14:39:34 -0300

    :tada: Primeiro Commit

---
+++ src/main/java/com/wistrip/WistripApplication.java
@@ -0,0 +1,13 @@
+package com.wistrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WistripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WistripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wistrip/WistripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wistrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WistripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
